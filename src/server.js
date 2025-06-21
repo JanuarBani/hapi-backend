0bcc9362833ab98f7944de0f6844529529a07bb7
JanuarBani <your.email@example.com>
2025-06-21 17:01:51 +0800

    Update fetch-api

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const init = async () => {
     host: 'localhost',
     routes: {
       cors: {
-        origin: ['http://localhost:9000'],
+        origin: ['https://nexa-path.vercel.app/'],
         credentials: true, // ✅ Dukung cookies/credentials
         headers: ['Accept', 'Content-Type', 'Authorization'], // ✅ Header yang diizinkan
         exposedHeaders: ['Accept', 'Content-Type', 'Authorization'], // ✅ Opsional
